4969f607d4dd6fa599bd8746545dc8c92ef9d8ce
Andrew_Ignatev <qwerty33226@gmail.com>
2026-08-21 11:34:24 +0300

    обновалён бек_3

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-// server.js – используем готовый Bearer-токен
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'; // отключаем проверку сертификата (для локальной разработки)
+// server.js – упрощённая версия с готовым Bearer-токеном
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'; // отключаем проверку сертификатов (для локальной разработки)
 
 require('dotenv').config();
 const express = require('express');
